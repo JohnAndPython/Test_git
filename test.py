a0293934a15570d93744acc7c179b99b4e9705fe
JohnJames <barthos@gmx.de>
2024-11-11 20:41:23 +0100

    Added for loop and avg calculation

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
 
 import random
 
-a = sum(random.randint(0, 1) for _ in range(10000))
+total :int = 0
 
-print(a)
+for _ in range(1000):
+    total += sum(random.randint(0, 1) for _ in range(10000))
+
+print(total / 1000)
